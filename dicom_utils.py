6b641ae44f92181c1fc97cefc377e3640991684d
radiplab <radiplab@gmail.com>
2021-02-04 17:04:18 -0600

    tests working

--- dicom_utils.py
+++ dicom_utils.py
@@ -3,6 +3,8 @@ import os
 import pydicom
 import shutil
 import time
+import zipfile
+from zipfile import ZipFile
 
 general_keep_list = [
                         'AccessionNumber',
@@ -133,25 +135,12 @@ def anonymize(dicom_path, output_path, preserve_list=list(), MRN=None, Accession
     start_time = None
     if verbose:
         start_time = time.time()
-        print("Beginning DICOM anonymization..", end = '')
+        print("Beginning anonymize of DICOM in " + dicom_path + " to " + output_path + "..", end = '')
 
+    # Create the output directory
     if os.path.exists(output_path):
         shutil.rmtree(output_path)
     os.mkdir(output_path)
-
-    # Build the output directory
-    output_dir = ''
-    if MRN is not None:
-        output_dir = output_dir + 'MRN' + MRN
-    if Accession is not None:
-        if output_dir != '':
-            output_dir = output_dir + '-'
-        output_dir = output_dir + 'Acc' + Accession
-
-    if os.path.exists(os.path.join(output_path, output_dir)):
-        shutil.rmtree(os.path.join(output_path, output_dir))
-    os.mkdir(os.path.join(output_path, output_dir))
-    output_path = os.path.join(output_path, output_dir)
 
     dicom_files = os.listdir(dicom_path)
     for dicom_file in dicom_files:
@@ -173,7 +162,6 @@ def anonymize(dicom_path, output_path, preserve_list=list(), MRN=None, Accession
     dicom_files = os.listdir(output_path)
     for dicom_file in dicom_files:
         if dicom_file.endswith('.dcm') and not dicom_file.startswith('KO') and not dicom_file.startswith('.DS_Store'):
-            #print(" - Processing " + dicom_file)
             ds = pydicom.filereader.dcmread(
                 os.path.join(output_path, dicom_file)
             )
@@ -232,3 +220,213 @@ def anonymize(dicom_path, output_path, preserve_list=list(), MRN=None, Accession
     if verbose:
         print('.done - time = ' + str(np.around((time.time() - start_time), decimals=1)) + 'sec')
     return output_path
+
+def filter_series_name(series):
+    """ Removes special characters from a series name
+
+    Parameters:
+        series (str): Series name to be filtered
+
+    Returns:
+        (str): Filtered series name
+    """    
+    series = str(series)
+    series = series.replace("/","")
+    series = series.replace("\\","")
+    series = series.replace(":","")
+    series = series.replace("*","")
+    series = series.replace("?","")
+    series = series.replace("\"","")
+    series = series.replace("<","")
+    series = series.replace(">","")
+    series = series.replace("|","")
+    series = series.replace(".","")
+    series = series.replace("+","")
+    # For some bizarre reason it doesn't work with "fps" in the filename
+    series = series.replace("fps", "")
+    
+    # Remove leading and trailing spaces
+    series = series.lstrip()
+    series = series.rstrip()  
+
+    return series
+
+def get_dicom_dataset(dicom_path):
+    """ Returns a pydicom dataset given a path to a folder containing dicom files (all files should be from the same study). 
+    Returns None if there is no valid DICOM file in the provided path
+
+    Parameters:
+        dicom_path (str):The full path to the directory containing DICOM files
+    """
+    dicom_dataset = None
+    dicom_files = os.listdir(dicom_path)
+    for dicom_file in dicom_files:
+        if not dicom_file.startswith('KO') and not dicom_file.startswith('.DS_Store'): 
+            dicom_dataset = pydicom.filereader.dcmread(os.path.join(dicom_path, dicom_file))
+            break
+    return dicom_dataset
+
+def output_metadata(dicom_path, output_path, verbose=False):
+    """ Outputs all metadata from a given folder of DICOM files. Any metadata different between DICOM files is output as well.
+
+    Parameters:
+        dicom_path (str):The full path to the directory containing DICOM files
+        output_path (str): The full path to the desired output text file, including the filename
+    """
+    start_time = None
+    if verbose:
+        start_time = time.time()
+        print("Beginning output_metadata from DICOM in " + dicom_path + " to " + output_path + "..", end = '')
+    
+    dicom_folder = os.path.basename(os.path.normpath(output_path))
+    
+    metadata_file = open(output_path, "w+")
+    metadata_file.write("\nFile -- Tag -- Keyword -- Name -- Value\n")
+    metadata_file.write("\n***** Case " + dicom_folder + " *****\n")
+    
+    # Create a dictionary with key of data_element.keyword - output data elements that are different
+    metadata_dict = {}
+    
+    dicom_files = os.listdir(dicom_path)                        
+    for dicom_file in dicom_files:
+        if dicom_file.endswith('.dcm') and not dicom_file.startswith('KO') and not dicom_file.startswith('.DS_Store'):
+            dicom_dataset = pydicom.filereader.dcmread(os.path.join(dicom_path, dicom_file))
+            for data_element in dicom_dataset:
+                write_data_element = False
+                if "pixel data" not in str(data_element.name).lower() and "overlay data" not in str(data_element.name).lower():
+                    if data_element.keyword in metadata_dict:
+                        # The data element has already been added once
+                        if data_element.value not in metadata_dict[data_element.keyword]:
+                            # The data element value is different though, so print again
+                            metadata_dict[data_element.keyword].append(data_element.value)
+                            write_data_element = True
+                    else:
+                        # The data element has not been added yet - create a list of 1 value and print
+                        metadata_dict[data_element.keyword] = [data_element.value]
+                        write_data_element = True
+                if write_data_element and data_element.keyword not in ['InstanceNumber', 'ImageOrientationPatient', 'ImagePositionPatient', 'SliceLocation', 'WindowCenter', 'WindowWidth', 'SOPInstanceUID']:
+                    metadata = str(dicom_file) + " | " + str(data_element.tag) + " | " + str(data_element.keyword) + " | " + str(data_element.name) + " | " + str(data_element.value) + "\n"
+                    metadata_file.write(metadata)
+
+    metadata_file.close()
+
+    if verbose:
+        print('.done - time = ' + str(np.around((time.time() - start_time), decimals=1)) + 'sec')    
+
+def zip(dicom_path, output_path, compression=zipfile.ZIP_DEFLATED, compresslevel=9, series=None, verbose=False):
+    """ Zips all *.dcm files within a given dicom_path. A different zip file is created for each series (SeriesDescription). Individual .dcm files are renamed based on their image number (InstanceNumber)
+
+    Parameters:
+        dicom_path (str):The full path to the directory containing DICOM files
+        output_path (str): The full path to the desired output zip file (not including filename)
+        compression: ZIP compression method to use when writing the archive, and should be ZIP_STORED, ZIP_DEFLATED, ZIP_BZIP2 or ZIP_LZMA; unrecognized values will cause NotImplementedError to be raised. If ZIP_DEFLATED, ZIP_BZIP2 or ZIP_LZMA is specified but the corresponding module (zlib, bz2 or lzma) is not available, RuntimeError is raised. The default is ZIP_STORED.
+        compresslevel: Controls the compression level to use when writing files to the archive. When using ZIP_STORED or ZIP_LZMA it has no effect. When using ZIP_DEFLATED integers 0 through 9 are accepted (see zlib for more information). When using ZIP_BZIP2 integers 1 through 9 are accepted (see bz2 for more information).
+        series (str): Optional series name. Only works if there is 1 series in the DICOM.
+    """
+    start_time = None
+    if verbose:
+        start_time = time.time()
+        print("Beginning zip of DICOM in " + dicom_path + " to " + output_path + "..", end = '')
+
+    zipfile_name = None
+    # Iterate through .dcm files - add each file to an archive based on its series name
+    
+    if os.path.exists(output_path):
+        shutil.rmtree(output_path)
+    os.mkdir(output_path)
+    
+    # Move DICOM into zip files based on series
+    series_dict = {}
+    for dcm_file in os.listdir(dicom_path):
+        if dcm_file.endswith('.dcm') and not dcm_file.startswith('KO') and not dcm_file.startswith('.DS_Store'):
+            dicom_dataset = pydicom.filereader.dcmread(os.path.join(dicom_path, dcm_file)) 
+            
+            series = ""
+            
+            series_instance_uid = dicom_dataset.SeriesInstanceUID
+            if series_instance_uid in series_dict:
+                # The series name has already been assigned
+                series = series_dict[series_instance_uid]
+            else:
+                # The series name has not been assigned
+                # Make sure there aren't 2 series with the same name
+                if "SeriesDescription" in dicom_dataset:
+                    # Make the series name the SeriesDescription
+                    series = str(dicom_dataset.SeriesDescription)
+                if series == "":
+                    if "SeriesNumber" in dicom_dataset:
+                        series = str(dicom_dataset.SeriesNumber)
+                if series == "":
+                    series = "Series"
+
+                # Eliminate special characters: / \ : * ? " < > |
+                series = filter_series_name(series)
+                    
+                if series in series_dict.values():
+                    # There are multiple series of the same name
+                    # Generate a unique name for the series
+                    new_series_name = ""
+                    ended = False
+                    i = 2
+                    while not ended:
+                        new_series_name = series + " " + str(i)
+                        i = i + 1
+                        if new_series_name not in series_dict.values():
+                            ended = True
+                    series_dict[series_instance_uid] = new_series_name
+                    series = new_series_name
+                else:
+                    # This is a new series - add to dictionary
+                    series_dict[series_instance_uid] = series                    
+
+            if "dose report" not in series.lower() and "screen save" not in series.lower() and "summary" not in series.lower():                                
+                zipfile_name = series + ".zip"
+                
+                with ZipFile(os.path.join(output_path, zipfile_name), 'a') as myzip:
+                    myzip.write(os.path.join(dicom_path, dcm_file), arcname=dcm_file)
+                    myzip.close()
+    
+    # Rename .dcm files for the image number (InstanceNumber) within each zip file
+    # Unzip the .dcm files into the same directory
+    for zip_file in os.listdir(output_path):
+        if zip_file.endswith('.zip'):
+            series = os.path.splitext(zip_file)[0]
+            with ZipFile(os.path.join(output_path, zip_file), 'a') as myzip:
+                myzip.extractall(output_path)
+                myzip.close()
+            os.remove(os.path.join(output_path, zip_file))      
+            
+            # key = filename, value = number with that filename - 1
+            dcm_filename_dict = {}
+            for dcm_file in os.listdir(output_path):
+                if dcm_file.endswith('.dcm') and not dcm_file.startswith('KO') and not dcm_file.startswith('.DS_Store'):
+                    dicom_dataset = pydicom.filereader.dcmread(os.path.join(output_path, dcm_file)) 
+
+                    if "dose report" not in series.lower() and "screen save" not in series.lower() and "summary" not in series.lower():                                
+                        zipfile_name = series + ".zip"
+                        
+                        # Name the .dcm file for the slice number (InstanceNumber)
+                        new_dcm_file = str(dicom_dataset.InstanceNumber).zfill(7)
+                        if new_dcm_file in dcm_filename_dict.keys():
+                            new_dcm_file_num = int(dcm_filename_dict[new_dcm_file]) + 1
+                            dcm_filename_dict[new_dcm_file] = new_dcm_file_num
+                            new_dcm_file = new_dcm_file + "-" + str(new_dcm_file_num)
+                        else:
+                            dcm_filename_dict[new_dcm_file] = 1
+                        
+                        new_dcm_file = new_dcm_file + ".dcm"
+                        
+                        if (dcm_file != new_dcm_file):
+                            os.rename(os.path.join(output_path, dcm_file), os.path.join(output_path, new_dcm_file))
+                        
+                        with ZipFile(os.path.join(output_path, zipfile_name), mode='a', compression=compression, compresslevel=compresslevel) as myzip:
+                            myzip.write(os.path.join(output_path, new_dcm_file), arcname=new_dcm_file)
+                            myzip.close()
+            
+            for dcm_file in os.listdir(output_path):
+                if dcm_file.endswith('.dcm'):
+                    os.remove(os.path.join(output_path, dcm_file))
+    if verbose:
+        print('.done - time = ' + str(np.around((time.time() - start_time), decimals=1)) + 'sec') 
+
+    return zipfile_name
---
+++ tests/test_dicom_utils.py
@@ -0,0 +1,56 @@
+import os
+import shutil
+import unittest
+
+import dicom_utils as du
+
+class TestDicomUtils(unittest.TestCase):
+
+    def test_integration_anonymize(self):
+        """
+        Integration test of DICOM anonymization method
+        """
+
+        test_dicom_path = r"./tests/data/test_ct_abdomen"
+        test_output_path = r"./tests/output"
+
+        if os.path.exists(test_output_path):
+            shutil.rmtree(test_output_path)
+        os.mkdir(test_output_path)
+
+        # Preserve the patient age, birth date, and gender. This may be desired for certain research, etc.
+        preserve_list = ("PatientAge", "PatientBirthDate", "PatientSex") 
+        anon_dicom_path = os.path.join(test_output_path, 'anon_dicom')
+
+        # Use all available options for anonymize:
+        # Specify a new MRN, Accession, patient name - also potentially useful for research
+        # Specify a new series description. There are actually 3 series in the test_ct_abdomen folder, 
+        # so this will test the zip method's ability to still put them in different zip files.
+        du.anonymize(test_dicom_path, anon_dicom_path, preserve_list=preserve_list, MRN='0000001', Accession='0000001', PatientName='LAST^FIRST', SeriesDescription='Anonymized CT Abdomen', verbose=True)
+
+        # Verify values in 1st DICOM file
+        ds = du.get_dicom_dataset(anon_dicom_path)
+        self.assertEqual(ds.PatientAge, '037Y')
+        self.assertEqual(ds.PatientBirthDate, '19791207')
+        self.assertEqual(ds.PatientSex, 'M')
+        self.assertEqual(ds.PatientID, '0000001')
+        self.assertEqual(ds.AccessionNumber, '0000001')
+        self.assertEqual(ds.PatientName, 'LAST^FIRST')
+        self.assertEqual(ds.SeriesDescription, 'Anonymized CT Abdomen')
+        self.assertEqual(ds.ReferringPhysicianName, 'anon')
+
+        # Output metadata for the DICOM - this will require some manual verification
+        anon_metadata_path = os.path.join(test_output_path, 'anon_metadata.txt')
+        du.output_metadata(anon_dicom_path, anon_metadata_path, verbose=True)
+
+        # Zip the anonymized DICOM. 3 different zip files should be created, one for each series
+        zip_output_path = os.path.join(test_output_path, 'zip')
+        du.zip(anon_dicom_path, zip_output_path, verbose=True)
+        zipfiles = os.listdir(zip_output_path)
+        self.assertEqual(zipfiles[0], 'Anonymized CT Abdomen.zip')
+        self.assertEqual(zipfiles[1], 'Anonymized CT Abdomen 2.zip')
+        self.assertEqual(zipfiles[2], 'Anonymized CT Abdomen 3.zip')
+
+if __name__ == '__main__':
+    unittest.main()        
+
